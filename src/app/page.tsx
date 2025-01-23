eae4ea20d21905e47e809680d68be4b0bd453a6a
gaurav ashok ghuge <guduughuge7@gmail.com>
2025-01-23 07:28:08 +0530

    update the repo

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import SelectedFile from "@/User/SelectedFile/SelectedFile";
+
+
+export default function Home() {
+  return (
+    <div className="">
+      
+      <SelectedFile />
+    </div>
+  );
+}
